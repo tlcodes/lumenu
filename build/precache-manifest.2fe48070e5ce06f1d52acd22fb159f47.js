b5081e7835918366c18f1d593739c1ed1a085ec4
tlcodes <imtrip@gmail.com>
2018-12-19 16:33:17 +0100

    add build

---
+++ build/precache-manifest.2fe48070e5ce06f1d52acd22fb159f47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ece909ca4c0686f707",
+    "url": "/lumenu/build/static/js/runtime~main.40ece909.js"
+  },
+  {
+    "revision": "d849950db05a438647ad",
+    "url": "/lumenu/build/static/js/main.d849950d.chunk.js"
+  },
+  {
+    "revision": "11dc499f5c73461a637e",
+    "url": "/lumenu/build/static/js/1.11dc499f.chunk.js"
+  },
+  {
+    "revision": "d849950db05a438647ad",
+    "url": "/lumenu/build/static/css/main.34f16644.chunk.css"
+  },
+  {
+    "revision": "f6007e588e6c1ed86e7b2a6525251a6f",
+    "url": "/lumenu/build/index.html"
+  }
+];
